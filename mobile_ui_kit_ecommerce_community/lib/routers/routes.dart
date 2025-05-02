d0d0c7766a48e91f7829eb940cd69bdccb633f98
bougdar <bougdarahmed264@gmail.com>
2025-05-02 15:30:00 +0100

    fix routes and flutter_screenutil

---
+++ mobile_ui_kit_ecommerce_community/lib/routers/routes.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:mobile_ui_kit_ecommerce_community/view/pages/home.dart';
+
+class AppRoutes {
+  static final routes = [
+    GetPage(name: '/HomePage', page: () => const HomePage()),
+  ];
+}
